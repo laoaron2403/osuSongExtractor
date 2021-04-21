67f88e626d345072d8372569972106f7ed059f67
sealnot123 <nottyzaza5531@gmail.com>
2021-04-21 23:28:33 +0700

    Add thoughts

--- Metadata.java
+++ Metadata.java
@@ -1,5 +1,22 @@
+import java.io.File;
+
+/**
+ * Objects containing metadata of the map
+ * @author sealnot123
+ */
 public class Metadata {
-    public static void main(String args[]){
-        
+    private String audioFileName;   // the name of mp3 file
+    private String title;           // the title of the song
+    private String unicodeTitle;    // the unicode title
+    private String artist;          // the artist of the song
+    private String unicodeArtist;   // the unicode artist
+    private String bgFileName;      // the name of wallpaper file
+
+    /**
+     * Constructor
+     * @param osuMap
+     */
+    public Metadata(File osuMap){
+
     }
 }
--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Gather all your mp3 files into a single folder from your /osu!/Song, rename them
 1. Extract .mp3 song files from /osu!/Songs folder and gather them onto a single folder
 2. Each .mp3 file has the name of the song (parsing from the map's metadata)
 3. (Optional) Each .mp3 file has the wallpaper of the map as its image.
+ 
+## Thoughts
+1. https://github.com/mpatric/mp3agic for editing metadata (including embed image)
+	1. How do I add dependencies to the project with Maven?
